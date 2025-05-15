e4363cec0907e30dfc1755a5c8cf28aa64a51fd5
ric <irgsessa@gmail.com>
2025-05-14 23:17:29 -0300

    se arregla fonts, para arreglar deploy en vercel

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+function Page() {
+  return (
+    <div>
+      <p>Customers Page.</p>
+    </div>
+  );
+}
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <div>
+      <h1></h1>
+      <p>Invoices page</p>
+    </div>
+  );
+}
+export default Page;
